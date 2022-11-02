d15741b08c75f07f43d595c12c315ca3a35d3b8d
WeirdGirlGamer <78986060+WeirdGirlGamer@users.noreply.github.com>
2022-11-02 15:21:15 -0400

    Add files via upload

---
+++ deploy-commands.js
@@ -0,0 +1,33 @@
+const fs = require('node:fs');
+const path = require('node:path');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord.js');
+const { clientID, guildID, token } = require('./config.json');
+
+const rest = new REST({ version: '10' }).setToken(token);
+
+
+const commands = [];
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	commands.push(command.data.toJSON());
+}
+
+(async () => {
+	try {
+		console.log('Started refreshing application (/) commands.');
+
+		await rest.put(
+			Routes.applicationGuildCommands(guildID),
+			{ body: commands },
+		);
+
+		console.log('Successfully reloaded application (/) commands.');
+	} catch (error) {
+		console.error(error);
+	}
+})();
